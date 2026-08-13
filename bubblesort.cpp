2f652814942fbf315579cb5a9197ad37a8335f3b
Local User <localuser@example.com>
2026-08-14 02:24:02 +0530

    brute force approach

---
+++ bubblesort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int>vec={12,4,16,10,9};
+    int n=vec.size();
+    for(int i=0; i<n-1; i++){
+        for(auto j=0; j<n-1; j++) {
+        if (vec[j]>vec[j+1]){
+            swap(vec[j],vec[j+1]);
+        }
+       
+    }
+    }
+    for(int i=0; i<vec.size(); i++){
+        cout<<vec[i]<<" ";
+    }
+    return 0;
+}
