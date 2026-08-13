e84fddcd90ff43324426f18f2cdd38853dfd6ee0
Jiya <jiyasood188@gmail.com>
2026-08-14 02:09:03 +0530

    Brute Force Approach

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -5,7 +5,7 @@ int main(){
     vector<int>vec={12,4,16,10,9};
     int n=vec.size();
     for(int i=0; i<n-1; i++){
-        for(auto j=0; j<n-1; j++){
+        for(auto j=0; j<n-1; j++) {
         if (vec[j]>vec[j+1]){
             swap(vec[j],vec[j+1]);
         }
