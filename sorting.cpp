29b984c193480c05d606aac442605c5c6b9736ac
Jiya <jiyasood188@gmail.com>
2026-08-16 01:39:57 +0530

    Bubble ssort brute force

--- sorting.cpp
+++ sorting.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 int main(){
-    vector<int>vec={12,4,16,10,9};
+    vector<int>vec={12,4,16,10,9}; 
     int n=vec.size();
     for(int i=0; i<n-1; i++){
         for(auto j=0; j<n-1; j++) {
